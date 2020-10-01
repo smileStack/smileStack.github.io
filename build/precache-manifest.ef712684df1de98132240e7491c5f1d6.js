91ee8bcc7f5a173ad350f8e2453471f92f433240
smileStack <ashley.carrasquel@gmail.com>
2020-10-01 00:18:19 -0400

    build

---
+++ build/precache-manifest.ef712684df1de98132240e7491c5f1d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be0c904339184478e57dd58b3ded20f2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e5d39ad6880a1c2b810f",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b256f43b8d023d6fd043",
+    "url": "./static/js/2.2fdb5ded.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2fdb5ded.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5d39ad6880a1c2b810f",
+    "url": "./static/js/main.6ec39b93.chunk.js"
+  },
+  {
+    "revision": "8cdb25f3ffb41440c4d8",
+    "url": "./static/js/runtime-main.e6c4d886.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
